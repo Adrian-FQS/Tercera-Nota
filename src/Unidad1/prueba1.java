935d5eb5759ab000e55eb7d950ab80dc568f0656
AdrianFQS <adrianfqs10@gmail.com>
2026-09-18 15:44:50 -0500

    prueba 1 y 2

---
+++ src/Unidad1/prueba1.java
@@ -0,0 +1,5 @@
+public class prueba1 {
+    public static void main(String[] args) {
+        System.out.println("Hola Adrian, este es mi primer ejercicio en Java");
+    }
+}
---
+++ src/unidad2/prueba2.java
@@ -0,0 +1,6 @@
+
+public class prueba2 {
+    public static void main(String[] args) {
+        System.out.println("Hola Adrian, este es mi primer ejercicio en Java");
+    }
+}
